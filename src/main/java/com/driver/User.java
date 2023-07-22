7873d2780ebebe381280ae60ae8c7723047105b8
Ajay Kumar <aky22896@gmail.com>
2023-07-22 16:40:55 +0530

    "first push"

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -3,4 +3,33 @@ package com.driver;
 public class User {
     private String name;
     private String mobile;
+
+
+    // No args constructor
+    public User(){
+
+    }
+
+    // All args constructor
+    public User(String name, String mobile){
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    // getter and setters
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
